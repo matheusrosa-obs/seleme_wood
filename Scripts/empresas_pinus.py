027dcafc1d87cd312b261808b3f25048b36680a1
matheusrwh <matheusrosa.lit@gmail.com>
2025-12-04 15:09:30 -0300

    Atts

--- Scripts/empresas_pinus.py
+++ Scripts/empresas_pinus.py
@@ -13,6 +13,27 @@ empresas = pd.read_excel(_resolve_path("Dados/Brutos/empresas_pinus.xlsx"))
 
 empresas.head()
 
+empresas_munic = empresas.groupby(
+    ['nm_municipio_1mai_sa', 'cd_mun_7d', 'nm_uf', 'sg_uf', 'nm_cnae_fiscal_principal']
+).size().reset_index(name='quantidade_empresas')
+
+print(empresas['nm_cnae_fiscal_principal'].unique())
+
+# Mapeando os setores para os dois grupos desejados
+setores_map = {
+    'Comércio varejista de materiais de construção não especificados anteriormente': 'Comércio varejista de materiais de construção',
+    'Comércio varejista de materiais de construção em geral': 'Comércio varejista de materiais de construção',
+    'Comércio atacadista de madeira e produtos derivados': 'Comércio atacadista de materiais de construção',
+    'Comércio varejista de madeira e artefatos': 'Comércio varejista de materiais de construção'
+}
+
+empresas['grupo_setor'] = empresas['nm_cnae_fiscal_principal'].map(setores_map).fillna(empresas['nm_cnae_fiscal_principal'])
+
+# Exemplo de agrupamento usando o novo grupo
+empresas_munic_grupo = empresas.groupby(
+    ['nm_municipio_1mai_sa', 'cd_mun_7d', 'nm_uf', 'sg_uf', 'grupo_setor']
+).size().reset_index(name='quantidade_empresas')
+
 lat_long = pd.read_csv(_resolve_path("Referências/dim_municipio_br.csv"))
 
 lat_long.head()
@@ -24,15 +45,13 @@ lat_long = lat_long[['cd_mun_7d',
                      'nu_latitude',
                      'nu_longitude']]
 
-empresas = empresas.merge(
+empresas_munic_grupo = empresas_munic_grupo.merge(
     lat_long,
-    left_on="cd_mun_ibge",
-    right_on="cd_mun_7d",
+    left_on=["cd_mun_7d"],
+    right_on=["cd_mun_7d"],
     how="left"
 )
 
-empresas.head()
+empresas_munic_grupo.head()
 
-empresas_munic = empresas.groupby(
-    ['nm_municipio_1mai_sa', 'cd_mun_7d', 'nm_uf', 'sg_uf', 'nm_cnae_fiscal_principal']
-).size().reset_index(name='quantidade_empresas')
+empresas_munic_grupo.to_csv(_resolve_path("Dados/Processados/empresas_pinus_munic.csv"), index=False)
